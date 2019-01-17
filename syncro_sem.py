cf7334dd387452857e59943385436d8b0d6231a0
ant <moldeskol@gmail.com>
2019-01-17 23:33:33 +0300

    Add script that implements synchronization using Semaphore

---
+++ syncro_sem.py
@@ -0,0 +1,19 @@
+from threading import Semaphore, Thread
+from time import sleep
+
+sem = Semaphore(1)
+seq = range(101)
+
+
+def printer(odd):
+    for i in seq:
+        with sem:
+            if i % 2 == odd:
+                print(i)
+        sleep(0.01)
+
+
+t1 = Thread(target=printer, args=(False,))
+t2 = Thread(target=printer, args=(True,))
+t1.start()
+t2.start()
