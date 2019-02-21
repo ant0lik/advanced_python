04f4da1343bd1ae5a414b0f75e5b3bb9a7004728
ant <anatolhg@hotmail.com>
2019-02-21 08:08:32 -0500

    PEP8 fixes

--- money/setup.py
+++ money/setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-        name="moneyor",
-        version='0.0.1',
-        author="Anatol",
-        author_email="author@example.com",
-        description="Money Handling Application",
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        url='https://github.com/ant0lik/advanced_python',
-        packages=setuptools.find_packages(),
+    name="moneyor",
+    version='0.0.1',
+    author="Anatol",
+    author_email="author@example.com",
+    description="Money Handling Application",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ant0lik/advanced_python',
+    packages=setuptools.find_packages(),
 )
